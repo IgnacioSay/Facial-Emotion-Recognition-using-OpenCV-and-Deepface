9179f9d35cd01e48a9efa8c8999869b55d6548b7
IgnacioSay <145888613+IgnacioSay@users.noreply.github.com>
2025-08-06 21:30:24 -0600

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,5 +1,20 @@
+import math
 import cv2
+import mediapipe as mp
+import numpy as np
+from xarm.wrapper import XArmAPI
 from deepface import DeepFace
+
+
+# Configure xArm
+arm = XArmAPI('192.168.1.201')  # Change for robot's IP
+arm.motion_enable(enable=True)
+arm.set_mode(0)
+arm.set_state(state=0)
+
+# Initial position
+x_pos = 200
+arm.set_position(246.6, -30.6, 236, 176.6, -2.2, -1.5, speed=50, wait=True)
 
 # Load face cascade classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -24,7 +39,6 @@ while True:
         # Extract the face ROI (Region of Interest)
         face_roi = rgb_frame[y:y + h, x:x + w]
 
-        
         # Perform emotion analysis on the face ROI
         result = DeepFace.analyze(face_roi, actions=['emotion'], enforce_detection=False)
 
@@ -34,6 +48,48 @@ while True:
         # Draw rectangle around face and label with predicted emotion
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+
+        if emotion == 'happy':
+          arm.tcp_acc = 2000
+          arm.tcp_speed = 300
+          arm.angle_acc = 2000
+          arm.angle_speed = 300
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+          arm.set_position(*[246.6, -30.6, 198.9, 176.6, -2.2, -1.5], speed=arm.tcp_speed, mvacc=arm.tcp_acc, radius=-1.0, wait=True)
+          arm.move_circle([271.6, -5.6, 198.9, 176.6, -2.2, -1.5], [271.6, -55.6, 198.9, 176.6, -2.2, -1.5], float(180) / 360 * 100, speed=arm.tcp_speed, mvacc=arm.tcp_acc, wait=True)
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+          #arm.set_cgpio_digital(0, 1, delay_sec=0)
+
+        if emotion == 'sad':
+          arm.tcp_acc = 2000
+          arm.tcp_speed = 300
+          arm.angle_acc = 2000
+          arm.angle_speed = 300
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+          arm.set_position(*[246.6, -30.6, 198.9, 176.6, -2.2, -1.5], speed=arm.tcp_speed, mvacc=arm.tcp_acc, radius=-1.0, wait=True)
+          arm.move_circle([271.6, -55.6, 198.9, 176.6, -2.2, -1.5], [271.6, -5.6, 198.9, 176.6, -2.2, -1.5], float(180) / 360 * 100, speed=arm.tcp_speed, mvacc=arm.tcp_acc, wait=True)
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+
+        if emotion == 'neutral':
+          arm.tcp_speed = 300
+          arm.tcp_acc = 2000
+          arm.angle_acc = 2000
+          arm.angle_speed = 300
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+          arm.set_position(*[246.6, -30.6, 198.9, 176.6, -2.2, -1.5], speed=arm.tcp_speed, mvacc=arm.tcp_acc, radius=-1.0, wait=True)
+          arm.set_position(*[306.6, -30.6, 198.9, 176.6, -2.2, -1.5], speed=arm.tcp_speed, mvacc=arm.tcp_acc, radius=-1.0, wait=True)
+          arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+
+        if emotion == 'surprise':
+            arm.tcp_acc = 2000
+            arm.tcp_speed = 300
+            arm.angle_acc = 2000
+            arm.angle_speed = 300
+            arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
+            arm.set_position(*[246.6, -30.6, 198.9, 176.6, -2.2, -1.5], speed=arm.tcp_speed, mvacc=arm.tcp_acc, radius=-1.0, wait=True)
+            arm.move_circle([271.6, -55.6, 198.9, 176.6, -2.2, -1.5], [271.6, -5.6, 198.9, 176.6, -2.2, -1.5], float(360) / 360 * 100, speed=arm.tcp_speed, mvacc=arm.tcp_acc, wait=True)
+            arm.set_servo_angle(angle=[-7.0, -35.1, -7.3, -4.9, 40.1, -1.7], speed=arm.angle_speed, mvacc=arm.angle_acc, wait=False, radius=0.0)
 
     # Display the resulting frame
     cv2.imshow('Real-time Emotion Detection', frame)
@@ -45,4 +101,3 @@ while True:
 # Release the capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
-
